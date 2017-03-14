29e50a98c42636e1858542edd4010604b7a7f166
Apisit Toompakdee <apmotive@gmail.com>
2017-03-15 08:55:37 +0900

    Added sample check new message

--- ChannelExample-Swift/ChannelExample-Swift/ViewController.swift
+++ ChannelExample-Swift/ChannelExample-Swift/ViewController.swift
@@ -10,25 +10,43 @@ import UIKit
 import Channel
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Channel.checkNewMessages { (numberOfNewMessages) in
+            if numberOfNewMessages > 0 {
+                let alert = UIAlertController(title: "", message: "You have \(numberOfNewMessages) new mesage", preferredStyle: .alert)
+                let viewAction = UIAlertAction(title: "View", style: .default, handler: { (_) in
+                    self.openChatView()
+                })
+                alert.addAction(viewAction)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+                    
+                })
+                alert.addAction(cancelAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    @IBAction func didTapContact(_ sender: Any) {
+    
+    func openChatView() {
         let userID = "ThisIsTheID"
         let userData = ["screen":"main",
                         "button":"contact"]
         let vc = Channel.chatViewController(withUserID: userID, userData: userData)
         vc.title = "Your title"
         self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTapContact(_ sender: Any) {
+        openChatView()
     }
 }
 
